35188e3bd5b2c8ed0bd638d4276ab991222ccacb
LakshikaDias <lakshika9504@gmail.com>
2022-10-19 17:35:14 +0530

    chnages

--- src/components/AllInOne.jsx
+++ src/components/AllInOne.jsx
@@ -1,16 +1,14 @@
-import { CheckIcon } from "@heroicons/react/24/outline"
+import { CheckIcon } from "@heroicons/react/24/outline";
 
 function AllInOne() {
   return (
     <div name="platforms" className="w-full my-32">
       <div className="max-w-[1240px] mx-auto px-2">
-        <h2 className="text-5xl font-bold text-center">
-          All-In-One Platform
-        </h2>
+        <h2 className="text-5xl font-bold text-center">All-In-One Platform</h2>
         <p className="text-2xl py-8 text-gray-500 text-center">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Eius repellendus nobis ratione vel perspiciatis suscipit,
-          dignissimos ipsam ipsa illum reiciendis.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
+          repellendus nobis ratione vel perspiciatis suscipit, dignissimos ipsam
+          ipsa illum reiciendis.
         </p>
 
         <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
@@ -19,10 +17,10 @@ function AllInOne() {
               <CheckIcon className="w-7 mr-4 text-green-600" />
             </div>
             <div>
-              <h3 className="font-bold text-lg">Notifications</h3>
+              <h3 className="font-bold text-lg">Notifications from Nike</h3>
               <p className="text-lg pt-2 pb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Impedit odio voluptates sed rerum? Cupiditate, unde.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                odio voluptates sed rerum? Cupiditate, unde.
               </p>
             </div>
           </div>
@@ -32,10 +30,12 @@ function AllInOne() {
               <CheckIcon className="w-7 mr-4 text-green-600" />
             </div>
             <div>
-              <h3 className="font-bold text-lg">Notifications</h3>
+              <h3 className="font-bold text-lg">
+                Notifications from Patagonia
+              </h3>
               <p className="text-lg pt-2 pb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Impedit odio voluptates sed rerum? Cupiditate, unde.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                odio voluptates sed rerum? Cupiditate, unde.
               </p>
             </div>
           </div>
@@ -45,10 +45,10 @@ function AllInOne() {
               <CheckIcon className="w-7 mr-4 text-green-600" />
             </div>
             <div>
-              <h3 className="font-bold text-lg">Notifications</h3>
+              <h3 className="font-bold text-lg">Notifications from Amazon</h3>
               <p className="text-lg pt-2 pb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Impedit odio voluptates sed rerum? Cupiditate, unde.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                odio voluptates sed rerum? Cupiditate, unde.
               </p>
             </div>
           </div>
@@ -58,10 +58,10 @@ function AllInOne() {
               <CheckIcon className="w-7 mr-4 text-green-600" />
             </div>
             <div>
-              <h3 className="font-bold text-lg">Notifications</h3>
+              <h3 className="font-bold text-lg">Notifications from Virgin</h3>
               <p className="text-lg pt-2 pb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Impedit odio voluptates sed rerum? Cupiditate, unde.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                odio voluptates sed rerum? Cupiditate, unde.
               </p>
             </div>
           </div>
@@ -71,10 +71,10 @@ function AllInOne() {
               <CheckIcon className="w-7 mr-4 text-green-600" />
             </div>
             <div>
-              <h3 className="font-bold text-lg">Notifications</h3>
+              <h3 className="font-bold text-lg">Notifications from Monocle</h3>
               <p className="text-lg pt-2 pb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Impedit odio voluptates sed rerum? Cupiditate, unde.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                odio voluptates sed rerum? Cupiditate, unde.
               </p>
             </div>
           </div>
@@ -84,10 +84,10 @@ function AllInOne() {
               <CheckIcon className="w-7 mr-4 text-green-600" />
             </div>
             <div>
-              <h3 className="font-bold text-lg">Notifications</h3>
+              <h3 className="font-bold text-lg">Notifications from Apple</h3>
               <p className="text-lg pt-2 pb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Impedit odio voluptates sed rerum? Cupiditate, unde.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                odio voluptates sed rerum? Cupiditate, unde.
               </p>
             </div>
           </div>
@@ -97,10 +97,10 @@ function AllInOne() {
               <CheckIcon className="w-7 mr-4 text-green-600" />
             </div>
             <div>
-              <h3 className="font-bold text-lg">Notifications</h3>
+              <h3 className="font-bold text-lg">Notifications from Cococola</h3>
               <p className="text-lg pt-2 pb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Impedit odio voluptates sed rerum? Cupiditate, unde.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                odio voluptates sed rerum? Cupiditate, unde.
               </p>
             </div>
           </div>
@@ -110,17 +110,19 @@ function AllInOne() {
               <CheckIcon className="w-7 mr-4 text-green-600" />
             </div>
             <div>
-              <h3 className="font-bold text-lg">Notifications</h3>
+              <h3 className="font-bold text-lg">
+                Notifications from Greyhound
+              </h3>
               <p className="text-lg pt-2 pb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Impedit odio voluptates sed rerum? Cupiditate, unde.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                odio voluptates sed rerum? Cupiditate, unde.
               </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default AllInOne
+export default AllInOne;
